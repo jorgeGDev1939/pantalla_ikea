e309f6ed800752395aa298afcc5688622e6261b5
jorgegp998 <jorgegp@live.cl>
2022-03-16 16:42:38 -0300

    first commit

---
+++ Nueva carpeta/main.js
@@ -0,0 +1,41 @@
+var btn_direccion_directa = false;
+var btn_direccion_reversa;
+
+if (btn_direccion_directa==true){
+    btn_direccion_reversa = false;
+    console.log(" el valor de btn es:"+btn_direccion_reversa)
+}
+else {
+    btn_direccion_reversa = true;
+    console.log(" el valor de btn es:"+btn_direccion_reversa)
+}
+
+var btn_partir_remoto
+var PE_remoto
+var SA02
+var SCF01
+var SCF02
+var SCL01
+var SCL02
+var SD01
+var SD02
+var SD03
+var SD04
+var SD05
+var SD06
+var SD07
+var SD08
+var SD09
+var SDL02
+var SDR02
+var SR01
+var SR02
+var SR03
+var SR04
+var SR05
+var SR06
+var SR07
+var SR08
+var SR09
+
+console.log(btn_direccion_reversa);
